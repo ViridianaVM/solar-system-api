c80c9e54e244371ce9c19b4f7adb46704f1c982d
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-04-30 13:49:52 -0700

    we are almost done

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
@@ -7,11 +8,14 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development
-    from app.models.planet import Planet
-    from .routes import planet_bp
-    app.register_blueprint(planet_bp)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+    
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from app.models.planet import Planet
+    from .routes import planets_bp
+    
+    app.register_blueprint(planets_bp)
     
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,25 @@
+from app import db
 from flask import Blueprint
+from flask import request, Blueprint, make_response
+from .models.planet import Planet
+
 
 planets_bp = Blueprint("planet", __name__, url_prefix="/planet")
 
-@planet_bp.route("/planet", methods=["GET"])
-def planet():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
+# @planet_bp.route("/planet", methods=["GET"])
+# def planet():
+#     my_beautiful_response_body = "Hello, World!"
+#     return my_beautiful_response_body
 
-@planet_bp.route("/planet/<id>", methods=["GET"])
-def planet():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
+# @planet_bp.route("/planet/<id>", methods=["GET"])
+# def planet():
+#     my_beautiful_response_body = "Hello, World!"
+#     return my_beautiful_response_body
 
-@planet_bp.route("", methods=["POST"])
+@planets_bp.route("", methods=["POST"])
 def planet():
-    new_planet = Planet(name = request.get_json(["name"]), description = request.get_json(["description"]))
+    req_body = request.get_json()
+    new_planet = Planet(name = req_body["name"], description = req_body["description"])
     db.session.add(new_planet)
     db.session.commit()
     my_beautiful_response_body = {
